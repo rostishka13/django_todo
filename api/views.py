992960504eb1c8e66267aae2fd290cee7c688ec7
Rostyk <rostishka1@gmail.com>
2021-04-20 12:38:17 +0300

    add crud via api

--- api/views.py
+++ api/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .serializers import TaskSerializer
+from base.models import Task
+import json
 # Create your views here.
 
 @api_view(["GET"])
@@ -16,3 +18,38 @@ def apiOverview(request):
 
 	return Response(api_urls)
 
+@api_view(['GET'])
+def taskList(request):
+	tasks = Task.objects.all()
+	serializer = TaskSerializer(tasks, many=True)
+	return Response(serializer.data)
+
+
+@api_view(['GET'])
+def taskDetail(request, pk):
+	task = Task.objects.get(id = pk)
+	serializer = TaskSerializer(task, many=False)
+	return Response(serializer.data)
+
+
+@api_view(['POST'])
+def taskCreate(request):
+
+	serializer = TaskSerializer(data = request.data)
+	if serializer.is_valid():
+		serializer.save()
+	return Response(serializer.data)
+
+@api_view(['POST'])
+def taskUpdate(request, pk):
+	task = Task.objects.get(id = pk)
+	serializer = TaskSerializer(instance=task, data=request.data)
+	if serializer.is_valid():
+		serializer.save()
+	return Response(serializer.data)
+
+@api_view(['DELETE'])
+def taskDelete(request, pk):
+	task = Task.objects.get(id = pk)
+	task.delete()
+	return Response("Your task successfully deleted")
